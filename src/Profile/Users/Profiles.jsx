cd4a0732d3efc6d6e4c0bb760d015372816e4310
Tring-Bharath <bharath.a@tringapps.com>
2025-03-25 14:59:25 +0530

    profile testing

--- src/Profile/Users/Profiles.jsx
+++ src/Profile/Users/Profiles.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LoggedOut from "./LoggedOut";
 import LoggedIn from "./LoggedIn";
 
 const Profile = () => {
-  const user = (localStorage.getItem("token"));
+  const user=JSON.parse(localStorage.getItem("user"));
+  
 
   return (
     <div className="d-flex w-100">
