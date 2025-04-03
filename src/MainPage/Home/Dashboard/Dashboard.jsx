031135a9fa416e330d07ba178b5c17001ef40c03
tring-bharath <bharath.a@tringapps.com>
2025-04-03 14:10:47 +0530

    infinte scroll

--- src/MainPage/Home/Dashboard/Dashboard.jsx
+++ src/MainPage/Home/Dashboard/Dashboard.jsx
@@ -67,7 +67,11 @@ const Dashboard = () => {
         ))}
       </Carousel>
       <History />
-      <InfiniteScroll dataLength={videos.length} next={handleGetAllVideos} hasMore={hasMore} loader={<h4 className="p-5">Loading Videos</h4>}
+      <InfiniteScroll
+        dataLength={videos.length}
+        next={handleGetAllVideos}
+        hasMore={hasMore}
+        loader={<h4 className="p-5">Loading Videos</h4>}
         className="infinite-scroll">
         <h1 className="ms-3">All Videos</h1>
         <div className="d-flex flex-wrap video-cards justify-content-center">
